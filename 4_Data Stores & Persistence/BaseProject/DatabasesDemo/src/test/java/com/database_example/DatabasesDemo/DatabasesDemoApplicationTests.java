b5aa7c1d5810207f5beca0b5824395304f4bd4fc
Karim Osama <Karim.Osama@thirdway.com>
2024-10-01 07:20:00 +0300

    First Example runs as expected!

---
+++ 4_Data Stores & Persistence/BaseProject/DatabasesDemo/src/test/java/com/database_example/DatabasesDemo/DatabasesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database_example.DatabasesDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
